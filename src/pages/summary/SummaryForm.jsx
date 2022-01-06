e2833b4ef05d69387a002ae5a03484cf4bc2c24e
Curtis <curtis_102@hotmail.com>
2022-01-06 01:13:31 -0800

    finished async issue

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -3,21 +3,70 @@ import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Popover from '@mui/material/Popover';
 
 export default function SummaryForm() {
   const [checked, setChecked] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   function onTacChange() {
     setChecked((prevChecked) => {
       return !prevChecked;
     });
   }
+
+  function getTermsText() {
+    return (
+      <Typography>
+        You agree to the{' '}
+        <span
+          aria-owns={open ? 'mouse-over-popover' : undefined}
+          aria-haspopup='true'
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+          style={{ color: 'blue' }}
+        >
+          Terms and Conditions
+        </span>
+        .
+      </Typography>
+    );
+  }
+
   return (
-    <Box sx={{ width: 300 }}>
-      <FormControlLabel
-        control={<Checkbox onChange={onTacChange} />}
-        label='Terms and Conditions'
-      />
+    <Box sx={{ width: 350 }}>
+      <FormControlLabel control={<Checkbox onChange={onTacChange} />} label={getTermsText()} />
+      <Popover
+        id='mouse-over-popover'
+        sx={{
+          pointerEvents: 'none',
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography sx={{ p: 1 }}>No stupid ice cream will be made morons so gtfo.</Typography>
+      </Popover>
       <Button variant='contained' disabled={!checked}>
         Confirm Order
       </Button>
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,9 +1,10 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 it('should have the checkbox and button disabled by default', () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'Terms and Conditions' });
+  const checkbox = screen.getByRole('checkbox', { name: /Terms and Conditions/i });
   const btn = screen.getByRole('button', { name: 'Confirm Order' });
   expect(checkbox).not.toBeChecked();
   expect(btn).toBeDisabled();
@@ -11,19 +12,36 @@ it('should have the checkbox and button disabled by default', () => {
 
 it('should enabled the button if the checkbox is checked', () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'Terms and Conditions' });
+  const checkbox = screen.getByRole('checkbox', { name: /Terms and Conditions/i });
   const btn = screen.getByRole('button', { name: 'Confirm Order' });
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(checkbox).toBeChecked();
   expect(btn).toBeEnabled();
 });
 
 it('should be disabled after the checkbox is unchecked after checked', () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'Terms and Conditions' });
+  const checkbox = screen.getByRole('checkbox', { name: /Terms and Conditions/i });
   const btn = screen.getByRole('button', { name: 'Confirm Order' });
-  fireEvent.click(checkbox);
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(btn).toBeDisabled();
+});
+
+it('should open a popover when hover on Terms of Conditions', async () => {
+  render(<SummaryForm />);
+  const popOverNull = screen.queryByText(/no stupid ice cream will be made morons/i);
+  expect(popOverNull).not.toBeInTheDocument();
+
+  const tacElm = screen.getByText(/terms and conditions/i);
+  userEvent.hover(tacElm);
+
+  const popOver = screen.queryByText(/no stupid ice cream will be made morons/i);
+  expect(popOver).toBeInTheDocument();
+
+  userEvent.unhover(tacElm);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no stupid ice cream will be made morons/i)
+  );
 });
